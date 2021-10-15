f165a80d420d8dc6a2ee5307267d8a3e6f2e89f9
Toni Engelhardt <toni88x@gmail.com>
2021-10-15 23:13:49 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  margin: 0;
+  padding: 0;
+}
+body {
+  @apply bg-gray-900;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-col">
+    <h1 class="text-6xl text-center font-bold">
+      <span class="text-transparent bg-gradient-to-r from-green-500 via-blue-500 to-purple-400 bg-clip-text">
+        Meta Data
+      </span>
+    </h1>
+    <div class="text-center">
+      <button class="px-4 py-1 my-8 text-blue-200 font-semibold rounded-full border border-blue-400 hover:(text-white bg-blue-400 border-transparent) focus:(outline-none ring-2 ring-blue-300)">
+        Connect
+      </button>
+    </div>
+    <!-- <p class="text-center text-xs text-gray-500">
+      This is a prototype, use at your own risk.
+    </p> -->
+  </div>
+</template>
